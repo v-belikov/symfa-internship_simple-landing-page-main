9625bd28116ca190170f366d5b3ec789e06eb33d
Eugene Kovgan <jugin.kovgan87@gmail.com>
2023-02-15 16:59:28 +0300

    feat: add fixed burger-menu

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,19 @@ import './styles.scss' // DO NOT REMOVE!
 const body = document.querySelector('.body')
 const burger_btn = document.querySelector('.burger_btn')
 const burger_menu = document.querySelector('.header_block__nav__burger')
+const burger__link = document.querySelectorAll('.header_block__nav__burger_list__link')
 
 burger_btn.addEventListener('click', () => {
   console.log('click')
   body.classList.toggle('lock')
   burger_btn.classList.toggle('active')
   burger_menu.classList.toggle('active')
+})
+
+Array.prototype.slice.call(burger__link).map((btn: any) => {
+  btn.addEventListener('click', (event: any) => {
+    body.classList.remove('lock')
+    burger_btn.classList.remove('active')
+    burger_menu.classList.remove('active')
+  })
 })
