10aad35b3a0ed9a97209db56b02cea078d7294f7
Eugene Kovgan <jugin.kovgan87@gmail.com>
2023-02-15 15:44:27 +0300

    feat: add burger-menu

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
-import './styles.scss'; // DO NOT REMOVE!
+import './styles.scss' // DO NOT REMOVE!
 /* WRITE YOUR CODE BELOW */
+const body = document.querySelector('.body')
+const burger_btn = document.querySelector('.burger_btn')
+const burger_menu = document.querySelector('.header_block__nav__burger')
+
+burger_btn.addEventListener('click', () => {
+  console.log('click')
+  body.classList.toggle('lock')
+  burger_btn.classList.toggle('active')
+  burger_menu.classList.toggle('active')
+})
