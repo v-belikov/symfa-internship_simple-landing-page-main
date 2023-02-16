83905f4ce0e52926130743620fb7488d526a80cf
Eugene Kovgan <jugin.kovgan87@gmail.com>
2023-02-16 16:02:05 +0300

    feat: add modal slider (without functional)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ const body = document.querySelector('.body')
 const burger_btn = document.querySelector('.burger_btn')
 const burger_menu = document.querySelector('.header_block__nav__burger')
 const burger__link = document.querySelectorAll('.header_block__nav__burger_list__link')
+const gallery__item_img = document.querySelectorAll('.gallery__main_block__item_img')
+const modal = document.querySelector('.modal')
+const modal_content = document.querySelector('.modal_content_img')
 
 burger_btn.addEventListener('click', () => {
   console.log('click')
@@ -14,8 +17,18 @@ burger_btn.addEventListener('click', () => {
 
 Array.prototype.slice.call(burger__link).map((btn: any) => {
   btn.addEventListener('click', (event: any) => {
-    body.classList.remove('lock')
+    body.classList.remove('lock')                                              //doesn't work !! WTF ?!
     burger_btn.classList.remove('active')
     burger_menu.classList.remove('active')
+  })
+})
+
+Array.prototype.slice.call(gallery__item_img).map((img: any) => {
+  img.addEventListener('click', (event: any) => {
+    body.classList.remove('lock')                                              //doesn't work !! WTF ?!
+    modal.classList.add('active')
+    body.classList.remove('lock')
+    let new_img = event.target
+    modal_content.appendChild(new_img)
   })
 })
