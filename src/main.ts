683d1ce164e9e738cf85076fb1a3ec5772c7a882
Eugene Kovgan <jugin.kovgan87@gmail.com>
2023-02-16 21:43:53 +0300

    feat: all ready (without slider)

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,65 @@
-import './styles.scss' // DO NOT REMOVE!
+// DO NOT REMOVE!
 /* WRITE YOUR CODE BELOW */
+import './styles.scss'
+
 const body = document.querySelector('.body')
 const burger_btn = document.querySelector('.burger_btn')
 const burger_menu = document.querySelector('.header_block__nav__burger')
 const burger__link = document.querySelectorAll('.header_block__nav__burger_list__link')
 const gallery__item_img = document.querySelectorAll('.gallery__main_block__item_img')
+// const gallery__main_block = document.querySelector('.gallery__main_block')
 const modal = document.querySelector('.modal')
-const modal_content = document.querySelector('.modal_content_img')
+// const modal_content = document.querySelector('.modal_content_img')
+const modal_content = document.querySelector('.modal_content_img') as HTMLElement
+const slider_btn_previous = document.querySelector('.slider_btn_previous')
+const slider_btn_next = document.querySelector('.slider_btn_next')
 
+//burger
 burger_btn.addEventListener('click', () => {
-  console.log('click')
   body.classList.toggle('lock')
   burger_btn.classList.toggle('active')
   burger_menu.classList.toggle('active')
 })
 
 Array.prototype.slice.call(burger__link).map((btn: any) => {
-  btn.addEventListener('click', (event: any) => {
-    body.classList.remove('lock')                                              //doesn't work !! WTF ?!
+  btn.addEventListener('click', () => {
+    body.classList.remove('lock')
     burger_btn.classList.remove('active')
     burger_menu.classList.remove('active')
   })
 })
 
-Array.prototype.slice.call(gallery__item_img).map((img: any) => {
+// slider
+let numberCurrentImg = 0
+let langsArrayOfImg = 0
+
+function activePicture(img: any) {
+  console.log(img)
+  modal_content.replaceWith(img)
+}
+
+Array.prototype.slice.call(gallery__item_img).map((img: any, index: any) => {
+  numberCurrentImg = index
+  langsArrayOfImg = gallery__item_img.length
   img.addEventListener('click', (event: any) => {
-    body.classList.remove('lock')                                              //doesn't work !! WTF ?!
-    modal.classList.add('active')
-    body.classList.remove('lock')
-    let new_img = event.target
-    modal_content.appendChild(new_img)
+    console.log(index)
+    const currentImg = gallery__item_img[index]
+    body.classList.toggle('lock')
+    modal.classList.toggle('active')
+    activePicture(currentImg)
   })
+})
+
+slider_btn_previous.addEventListener('click', () => {
+  --numberCurrentImg
+  if (numberCurrentImg <= 0) { numberCurrentImg = 0 }
+  console.log(numberCurrentImg)
+  console.log('slider_btn_previous')
+})
+
+slider_btn_next.addEventListener('click', () => {
+  ++numberCurrentImg
+  if (numberCurrentImg >= langsArrayOfImg) { numberCurrentImg = 0 }
+  console.log(numberCurrentImg)
+  console.log('slider_btn_next')
 })
