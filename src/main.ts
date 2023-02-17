2e2d988931cd0cf25266061b0e44399ff05b7ed1
Eugene Kovgan <jugin.kovgan87@gmail.com>
2023-02-17 13:50:15 +0300

    feat: all ready

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,77 @@
 // DO NOT REMOVE!
 /* WRITE YOUR CODE BELOW */
-import './styles.scss'
+import './styles.scss';
 
-const body = document.querySelector('.body')
-const burger_btn = document.querySelector('.burger_btn')
-const burger_menu = document.querySelector('.header_block__nav__burger')
-const burger__link = document.querySelectorAll('.header_block__nav__burger_list__link')
-const gallery__item_img = document.querySelectorAll('.gallery__main_block__item_img')
-// const gallery__main_block = document.querySelector('.gallery__main_block')
-const modal = document.querySelector('.modal')
-// const modal_content = document.querySelector('.modal_content_img')
-const modal_content = document.querySelector('.modal_content_img') as HTMLElement
-const slider_btn_previous = document.querySelector('.slider_btn_previous')
-const slider_btn_next = document.querySelector('.slider_btn_next')
+const body = document.querySelector('.body') as HTMLElement;
+const burgerBtn = document.querySelector('.burger_btn') as HTMLElement;
+const burgerMenu = document.querySelector('.header_block__nav__burger') as HTMLElement;
+const burgerLink = document.querySelectorAll('.header_block__nav__burger_list__link');
+const galleryItemImg = document.querySelectorAll('.gallery__main_block__item_img');
+const modal = document.querySelector('.modal') as HTMLElement;
+const modalContent = document.querySelector('.modal_content_img') as HTMLElement;
+const sliderBtnPrevious = document.querySelector('.slider_btn_previous') as HTMLElement;
+const sliderBtnNext = document.querySelector('.slider_btn_next') as HTMLElement;
 
-//burger
-burger_btn.addEventListener('click', () => {
-  body.classList.toggle('lock')
-  burger_btn.classList.toggle('active')
-  burger_menu.classList.toggle('active')
-})
+burgerBtn.addEventListener('click', () => {
+  body.classList.toggle('lock');
+  burgerBtn.classList.toggle('active');
+  burgerMenu.classList.toggle('active');
+});
 
-Array.prototype.slice.call(burger__link).map((btn: any) => {
+Array.prototype.slice.call(burgerLink).map((btn: HTMLElement) => {
   btn.addEventListener('click', () => {
-    body.classList.remove('lock')
-    burger_btn.classList.remove('active')
-    burger_menu.classList.remove('active')
-  })
-})
+    body.classList.remove('lock');
+    burgerBtn.classList.remove('active');
+    burgerMenu.classList.remove('active');
+  });
+});
 
 // slider
-let numberCurrentImg = 0
-let langsArrayOfImg = 0
+// косяк с работой самого слайдера, суть такова:
+// имеем массив с путями к самим картинкам. При клике на картинку получаем порядковый номер этой картинки, //
+// что позволяет нарисвать ее в модальном-окне-слайдере, перебрав этот массив.
+// НО все картинки при работе вебпака(видимо) получают дополнительный уникальный класс?(добавляется к пути). Не совсем ясно, что делать в таком случае.
+// обращаясь по путям с этим классом картинку получить у меня не получается. НО и без него тоже.  //
+let numberCurrentImg = 0;
+let langsArrayOfImg = 0;
+const arrayOfPictures: Array<string> = [];
 
-function activePicture(img: any) {
-  console.log(img)
-  modal_content.replaceWith(img)
+function activePicture(imgIndex: number) {
+  console.log(numberCurrentImg);
+  console.log(arrayOfPictures[imgIndex]);
+  // console.log(arrayOfPictures)
+  // modalContent.innerHTML += `<img src="./assets/img/png/IMG_${arrayOfPictures[imgIndex]}.png" alt="">`
+  modalContent.innerHTML += `<img src="${arrayOfPictures[imgIndex]}" alt="">`;
 }
 
-Array.prototype.slice.call(gallery__item_img).map((img: any, index: any) => {
-  numberCurrentImg = index
-  langsArrayOfImg = gallery__item_img.length
-  img.addEventListener('click', (event: any) => {
-    console.log(index)
-    const currentImg = gallery__item_img[index]
-    body.classList.toggle('lock')
-    modal.classList.toggle('active')
-    activePicture(currentImg)
-  })
-})
+Array.prototype.slice.call(galleryItemImg).map((img: HTMLElement, index: number) => {
+  arrayOfPictures.push(img.querySelector('img').getAttribute('src'));
+  // arrayOfPictures.push(img.querySelector('img').getAttribute('src').slice(11, 15));
+  numberCurrentImg = index;
+  langsArrayOfImg = galleryItemImg.length;
+  img.addEventListener('click', () => {
+    body.classList.toggle('lock');
+    modal.classList.toggle('active');
+    activePicture(numberCurrentImg);
+  });
+});
 
-slider_btn_previous.addEventListener('click', () => {
-  --numberCurrentImg
-  if (numberCurrentImg <= 0) { numberCurrentImg = 0 }
-  console.log(numberCurrentImg)
-  console.log('slider_btn_previous')
-})
+sliderBtnPrevious.addEventListener('click', () => {
+  numberCurrentImg -= 1;
 
-slider_btn_next.addEventListener('click', () => {
-  ++numberCurrentImg
-  if (numberCurrentImg >= langsArrayOfImg) { numberCurrentImg = 0 }
-  console.log(numberCurrentImg)
-  console.log('slider_btn_next')
-})
+  if (numberCurrentImg <= 0) {
+    numberCurrentImg = 0;
+  }
+
+  activePicture(numberCurrentImg);
+});
+
+sliderBtnNext.addEventListener('click', () => {
+  numberCurrentImg += 1;
+
+  if (numberCurrentImg >= langsArrayOfImg) {
+    numberCurrentImg = 0;
+  }
+
+  activePicture(numberCurrentImg);
+});
