70443f49908aa3c49472434d6db8c1579d4b87ac
Dzmitry Mikhayevich <dima.mikhayevich@mail.ru>
2023-02-18 14:11:20 +0300

    feat: slider refactoring: styles.scss

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,30 @@
-import './styles.scss'; // DO NOT REMOVE!
-/* WRITE YOUR CODE BELOW */
+import './styles.scss';
+
+const modal = document.querySelector('.modal');
+const modalBody = document.querySelector('.modal-body');
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
+const images = document.querySelectorAll('.gallery-item');
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
+
+images.forEach((elem, index) => {
+  elem.addEventListener('click', () => {
+    prevBtn.addEventListener('click', () => {
+      index === 0 ? (index = images.length - 1) : (index -= 1);
+      modalBody.innerHTML = images[index].outerHTML;
+    });
+
+    nextBtn.addEventListener('click', () => {
+      index === images.length - 1 ? (index = 0) : (index += 1);
+      modalBody.innerHTML = images[index].outerHTML;
+    });
+
+    modal.style.display = 'block';
+    modalBody.innerHTML = images[index].outerHTML;
+  });
+});
